635f675b67be2b4ae6fdf6f3a16726e372fbbb81
Anton <anton.wiblishauser@gmx.de>
2022-10-20 23:21:38 +0200

    fix telekinesis check

--- src/main/java/dev/nyon/telekinesis/check/TelekinesisCheck.java
+++ src/main/java/dev/nyon/telekinesis/check/TelekinesisCheck.java
@@ -17,7 +17,7 @@ public class TelekinesisCheck {
         if (source.getEntity() instanceof Player) player = (Player) source.getEntity();
         if (entity.getKillCredit() instanceof Player) player = (Player) entity.getKillCredit();
         if (player == null) return new Pair<>(true, null);
-        ArrayList<ItemStack> acceptedItems = (ArrayList<ItemStack>) List.of(player.getOffhandItem(), player.getInventory().getSelected());
+        ArrayList<ItemStack> acceptedItems = new ArrayList<>(List.of(player.getOffhandItem(), player.getInventory().getSelected()));
         player.getArmorSlots().forEach(acceptedItems::add);
         return new Pair<>(
             acceptedItems.stream().noneMatch(item -> EnchantmentHelper.getEnchantments(item).containsKey(TelekinesisKt.getTelekinesis())),
