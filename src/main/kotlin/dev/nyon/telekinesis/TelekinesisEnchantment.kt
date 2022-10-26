9801dded0e1187c3feaecce3ffe0ac53d15cfc6f
Anton <anton.wiblishauser@gmx.de>
2022-10-27 00:21:56 +0200

    remove silk implementation

--- src/main/kotlin/dev/nyon/telekinesis/TelekinesisEnchantment.kt
+++ src/main/kotlin/dev/nyon/telekinesis/TelekinesisEnchantment.kt
@@ -1,12 +1,12 @@
 package dev.nyon.telekinesis
 
 import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.Style
 import net.minecraft.world.damagesource.DamageSource
 import net.minecraft.world.entity.EquipmentSlot
 import net.minecraft.world.entity.MobType
 import net.minecraft.world.item.enchantment.Enchantment
 import net.minecraft.world.item.enchantment.EnchantmentCategory
-import net.silkmc.silk.core.text.literalText
 
 class TelekinesisEnchantment : Enchantment(
     Rarity.RARE, EnchantmentCategory.BREAKABLE, listOf(EquipmentSlot.OFFHAND, EquipmentSlot.MAINHAND).toTypedArray()
@@ -21,5 +21,6 @@ class TelekinesisEnchantment : Enchantment(
     override fun getDamageProtection(i: Int, damageSource: DamageSource): Int = 0
     override fun getDamageBonus(i: Int, mobType: MobType): Float = 0F
 
-    override fun getFullname(i: Int): Component = literalText("Telekinesis") { color = 0xFFB64C }
+    override fun getFullname(i: Int): Component =
+        Component.literal("Telekinesis").withStyle(Style.EMPTY.withColor(0xFFB64C))
 }
