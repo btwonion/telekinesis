36fe9026db30501c3793cf79974ce44d5f84949e
Anton <anton.wiblishauser@gmx.de>
2022-11-04 18:41:32 +0100

    rename telekinesisCheck to telekinesisUtils

--- src/main/java/dev/nyon/telekinesis/check/TelekinesisUtils.java
+++ src/main/java/dev/nyon/telekinesis/check/TelekinesisUtils.java
@@ -3,15 +3,18 @@ package dev.nyon.telekinesis.check;
 import dev.nyon.telekinesis.TelekinesisKt;
 import kotlin.Pair;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-public class TelekinesisCheck {
+public class TelekinesisUtils {
     public static Pair<Boolean, Player> hasNoTelekinesis(DamageSource source, LivingEntity entity) {
         Player player = null;
         if (source.getEntity() instanceof Player) player = (Player) source.getEntity();
@@ -23,5 +26,15 @@ public class TelekinesisCheck {
             acceptedItems.stream().noneMatch(item -> EnchantmentHelper.getEnchantments(item).containsKey(TelekinesisKt.getTelekinesis())),
             player
         );
+    }
+
+    public static void addXPToPlayer(Player player, Integer xp) {
+        player.giveExperiencePoints(xp);
+        Map.Entry<EquipmentSlot, ItemStack> entry = EnchantmentHelper.getRandomItemWith(Enchantments.MENDING, player, ItemStack::isDamaged);
+        if (entry != null) {
+            ItemStack itemStack = entry.getValue();
+            int j = Math.min(xp * 2, itemStack.getDamageValue());
+            itemStack.setDamageValue(itemStack.getDamageValue() - j);
+        }
     }
 }
