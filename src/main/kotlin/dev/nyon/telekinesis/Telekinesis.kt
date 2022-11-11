fbb51f601a24da2d7820c44088bbe192739f4a0d
Anton <anton.wiblishauser@gmx.de>
2022-11-11 19:32:17 +0100

    fix registries

--- src/main/kotlin/dev/nyon/telekinesis/Telekinesis.kt
+++ src/main/kotlin/dev/nyon/telekinesis/Telekinesis.kt
@@ -3,6 +3,7 @@ package dev.nyon.telekinesis
 import dev.nyon.telekinesis.config.config
 import dev.nyon.telekinesis.config.loadConfig
 import net.minecraft.core.Registry
+import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.resources.ResourceLocation
 
 val telekinesis = TelekinesisEnchantment()
@@ -10,6 +11,6 @@ val telekinesis = TelekinesisEnchantment()
 class Telekinesis {
     fun init() {
         loadConfig()
-        if (config.enchantment) Registry.register(Registry.ENCHANTMENT, ResourceLocation("telekinesis", "telekinesis"), telekinesis)
+        if (config.enchantment) Registry.register(BuiltInRegistries.ENCHANTMENT, ResourceLocation("telekinesis", "telekinesis"), telekinesis)
     }
 }
