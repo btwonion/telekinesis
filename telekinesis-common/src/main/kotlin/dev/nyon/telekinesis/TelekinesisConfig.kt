eefc2cc3e3d5645bac08a4f625dbd38760a8f5cd
Anton <anton.wiblishauser@gmx.de>
2023-03-10 23:58:03 +0100

    remove toml comments

--- telekinesis-common/src/main/kotlin/dev/nyon/telekinesis/TelekinesisConfig.kt
+++ telekinesis-common/src/main/kotlin/dev/nyon/telekinesis/TelekinesisConfig.kt
@@ -1,35 +1,32 @@
 package dev.nyon.telekinesis
 
 import com.akuleshov7.ktoml.Toml
-import com.akuleshov7.ktoml.annotations.TomlComments
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.serializer
 import java.nio.file.Path
 import kotlin.io.path.readText
 import kotlin.io.path.writeText
 
 @Serializable
 data class TelekinesisConfig(
-    @TomlComments(
+    /*@TomlComments(
         lines = ["decides whether telekinesis should work without the enchantment or not"], inline = "false by default"
-    )
+    )*/
     val onByDefault: Boolean = false,
-    @TomlComments(lines = ["decides if the enchantment is enabled"], inline = "true by default")
+    //@TomlComments(lines = ["decides if the enchantment is enabled"], inline = "true by default")
     val enchantment: Boolean = true,
-    @TomlComments(lines = ["decides if telekinesis is enabled for block drops"], inline = "true by default")
+    //@TomlComments(lines = ["decides if telekinesis is enabled for block drops"], inline = "true by default")
     val blockDrops: Boolean = true,
-    @TomlComments(lines = ["decides if telekinesis is enabled for exp drops"], inline = "true by default")
+    //@TomlComments(lines = ["decides if telekinesis is enabled for exp drops"], inline = "true by default")
     val expDrops: Boolean = true,
-    @TomlComments(lines = ["decides if telekinesis is enabled for mob drops"], inline = "true by default")
+    //@TomlComments(lines = ["decides if telekinesis is enabled for mob drops"], inline = "true by default")
     val mobDrops: Boolean = true,
-    @TomlComments(
+    /*@TomlComments(
         lines = ["decides if telekinesis is enabled for entity drops such as boats and minecarts"],
         inline = "true by default"
-    )
+    )*/
     val entityDrops: Boolean = true,
-    @TomlComments("decides if telekinesis is enabled for shearing drops", inline = "true by default")
+    //@TomlComments("decides if telekinesis is enabled for shearing drops", inline = "true by default")
     val shearingDrops: Boolean = true
 )
 
