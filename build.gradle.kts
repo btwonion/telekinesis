cd141c10f2e032524747bd70b1f4510c0a9a4408
Anton <anton.wiblishauser@gmx.de>
2023-08-10 18:14:31 +0200

    update minotaur

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.0"
     kotlin("plugin.serialization") version "1.9.0"
-    id("com.modrinth.minotaur") version "2.8.1" apply (false)
+    id("com.modrinth.minotaur") version "2.8.2" apply (false)
     id("com.github.breadmoirai.github-release") version "2.4.1" apply (false)
 
     id("fabric-loom") version "1.3-SNAPSHOT" apply (false)
