b3ed246ebb0fd26a1320274a0db9c95b60d3c227
Anton <anton.wiblishauser@gmx.de>
2023-09-11 21:30:21 +0200

    make config work

--- telekinesis-paper/src/main/kotlin/dev/nyon/telekinesis/TelekinesisConfig.kt
+++ telekinesis-paper/src/main/kotlin/dev/nyon/telekinesis/TelekinesisConfig.kt
@@ -2,10 +2,12 @@ package dev.nyon.telekinesis
 
 import com.akuleshov7.ktoml.Toml
 import com.akuleshov7.ktoml.TomlInputConfig
+import com.akuleshov7.ktoml.TomlOutputConfig
 import com.akuleshov7.ktoml.annotations.TomlComments
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import java.nio.file.Path
+import kotlin.io.path.notExists
 import kotlin.io.path.readText
 import kotlin.io.path.writeText
 
@@ -15,35 +17,33 @@ data class TelekinesisConfig(
     @TomlComments(
         "Uncomment this by removing '# ' to block functionality for those who don't have the required permission.",
         "onByDefaultPermissionRequirement = 'permission'"
-    )
-    var onByDefaultPermissionRequirement: String? = null,
+    ) var onByDefaultPermissionRequirement: String? = null,
     var onlyOnSneak: Boolean = false,
     var enchantment: Boolean = true,
     var blockDrops: Boolean = true,
-     @TomlComments(
+    @TomlComments(
         "blockDropsPermissionRequirement = 'permission'"
-    )
-    var blockDropsPermissionRequirement: String? = null,
+    ) var blockDropsPermissionRequirement: String? = null,
     var expDrops: Boolean = true,
-     @TomlComments(
+    @TomlComments(
         "expDropsPermissionRequirement = 'permission'"
-    )
-    var expDropsPermissionRequirement: String? = null,
+    ) var expDropsPermissionRequirement: String? = null,
     var entityDrops: Boolean = true,
     @TomlComments(
         "entityDropsPermissionRequirement = 'permission'"
-    )
-    var entityDropsPermissionRequirement: String? = null
+    ) var entityDropsPermissionRequirement: String? = null
 )
 
 var config: TelekinesisConfig = TelekinesisConfig()
 lateinit var configPath: Path
-val toml = Toml(inputConfig = TomlInputConfig(ignoreUnknownNames = true))
+val toml = Toml(
+    inputConfig = TomlInputConfig(ignoreUnknownNames = true), outputConfig = TomlOutputConfig(ignoreNullValues = false)
+)
 
 fun saveConfig() = configPath.writeText(toml.encodeToString(TelekinesisConfig.serializer(), config))
 
 fun loadConfig() {
-    if (configPath.readText().isEmpty()) {
+    if (configPath.notExists() || configPath.readText().isEmpty()) {
         saveConfig()
         return
     }
